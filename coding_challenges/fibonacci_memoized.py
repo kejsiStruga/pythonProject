d6493cf13ed53627fad459dbfc8801a2f4d3b6f6
kejsiStruga <kejsi.struga@fshnstudent.info>
2021-06-27 19:59:48 +0200

    fibonacci with memoization (dp)

---
+++ coding_challenges/fibonacci_memoized.py
@@ -0,0 +1,13 @@
+
+
+def fib_memo(n, fib_cache):
+    if n == 0 or n == 1:
+        fib_cache[n] = n
+    if n not in fib_cache:
+        fib_cache[n] = fib_memo(n - 1, fib_cache) + fib_memo(n - 2, fib_cache)
+    return fib_cache[n]
+
+
+if __name__ == "__main__":
+    for i in range(1, 101):
+        print(i, ": ", fib_memo(i, {}))
