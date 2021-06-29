bcbf9e85d58d22b25d3bd7683a41818c4e5698e7
kejsiStruga <kejsi.struga@fshnstudent.info>
2021-06-29 12:02:55 +0200

    Array Left rotation optimization Hackerrank solution

--- coding_challenges/array_left_rotation.py
+++ coding_challenges/array_left_rotation.py
@@ -8,7 +8,25 @@ def array_left_rotation(a, d):
     return a
 
 
+def array_left_rotation_optimized(a, d):
+    rotated_array = []
+    arr_length = len(a)
+    rotate_idx = d
+
+    while rotate_idx < arr_length:
+        rotated_array.append(a[rotate_idx])
+        rotate_idx = rotate_idx + 1
+
+    rotate_idx = 0
+
+    while rotate_idx < d:
+        rotated_array.append(a[rotate_idx])
+        rotate_idx = rotate_idx + 1
+
+    return rotated_array
+
+
 if __name__ == '__main__':
     arr = [1, 2, 3, 4, 5]
     # 1 rotation -> [2, 3, 4, 5, 1]
-    print(array_left_rotation(arr, 2))
+    print(array_left_rotation_optimized(arr, 4))
