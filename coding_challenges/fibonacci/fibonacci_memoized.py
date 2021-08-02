c6492cc40a31a9a47605f18b576ad3b1370c3a2a
kejsiStruga <kejsi.struga@fshnstudent.info>
2021-08-02 12:23:10 +0200

    refactor naming

--- coding_challenges/fibonacci/fibonacci_memoized.py
+++ coding_challenges/fibonacci/fibonacci_memoized.py
@@ -3,14 +3,14 @@ O(n) big-o complexity
 """
 
 
-def fib_memo(n, fib_cache):
+def fib_memoization(n, fib_memo):
     if n == 0 or n == 1:
-        fib_cache[n] = n
-    if n not in fib_cache:
-        fib_cache[n] = fib_memo(n - 1, fib_cache) + fib_memo(n - 2, fib_cache)
-    return fib_cache[n]
+        fib_memo[n] = n
+    if n not in fib_memo:
+        fib_memo[n] = fib_memo(n - 1, fib_memo) + fib_memo(n - 2, fib_memo)
+    return fib_memo[n]
 
 
 if __name__ == "__main__":
     for i in range(1, 101):
-        print(i, ": ", fib_memo(i, {}))
+        print(i, ": ", fib_memoization(i, {}))
